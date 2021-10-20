c6190d95d091b147d268d5aa3be5cb48be36d479
S A Saharukh <sasaharukh@gmail.com>
2021-10-20 20:03:43 +0600

    view user

--- controller/users.controller.php
+++ controller/users.controller.php
@@ -1,4 +1,22 @@
 <?php
     $string = file_get_contents("../model/admin.model.json");
     $adminData = json_decode($string, true);
+
+
+
+
+
+    $string = file_get_contents("../model/doctor.model.json");
+    $doctorData = json_decode($string, true);
+
+
+
+
+    $string = file_get_contents("../model/patient.model.json");
+    $patentData = json_decode($string, true);
+
+
+
+    $string = file_get_contents("../model/shopmanager.model.json");
+    $shopManagerData = json_decode($string, true);
 ?>
--- view/manageUsers.view.php
+++ view/manageUsers.view.php
@@ -12,21 +12,101 @@
     <?php include("../controller/users.controller.php") ?>
     <br>
     <br>
+    <input type="button" value="Add New Admin">
+    <hr>
     <h2>ADMINs</h2>
     <?php
     echo "<table border = 1>";
-    
+    echo  '<th>Fist Name</th><th>Last Name</th> <th>username</th> <th>Email</th>';
     foreach($adminData as $user) {
+        echo "<form>";
         echo "<tr>";
         echo "<td>".$user['firstName']."</td>";
         echo "<td>".$user['lastName']."</td>";
+        echo "<td>".$user['username']."</td>";
         echo "<td>".$user['email']."</td>";
-    
+        $email = $user['email'];
+        echo "<td> <button type='submit' value='$email'> edit</button> </td>";
         echo "</tr>";
+        echo "</form>";
        
     }
     
-echo "</table>";
-    ?>
+    echo "</table>";
+        ?>
+
+    <h2>Doctors</h2>
+        <?php
+        echo "<table border = 1>";
+        echo  '<th>Fist Name</th><th>Last Name</th> <th>Email</th> <th>Age</th> <th>Degree</th> <th>Department</th> <th>Fee </th>';
+        foreach($doctorData as $doc) {
+            echo "<form>";
+            echo "<tr>";
+            echo "<td>".$doc['firstName']."</td>";
+            echo "<td>".$doc['lastName']."</td>";
+            echo "<td>".$doc['email']."</td>";
+            echo "<td>".$doc['age']."</td>";
+            echo "<td>".$doc['degree']."</td>";
+            echo "<td>".$doc['department']."</td>";
+            echo "<td>".$doc['visitingFee']."</td>";
+            $email = $doc['email'];
+            echo "<td> <button type='submit' value='$email'> edit</button> </td>";
+            echo "</tr>";
+            echo "</form>";
+        
+        }
+        
+        echo "</table>";
+            ?>
+
+
+<h2>Patients</h2>
+        <?php
+        echo "<table border = 1>";
+        echo  '<th>First Name</th><th>Last Name</th> <th>email</th><td>Age</th><td>Gender</td><td>Address<td>';
+        foreach($patentData as $doc) {
+            echo "<form>";
+            echo "<tr>";
+            echo "<td>".$doc['firstName']."</td>";
+            echo "<td>".$doc['lastName']."</td>";
+            echo "<td>".$doc['email']."</td>";
+            echo "<td>".$doc['age']."</td>";
+            echo "<td>".$doc['gender']."</td>";
+            echo "<td>".$doc['address']."</td>";
+            $email = $doc['email'];
+            echo "<td> <button type='submit' value='$email'> edit</button> </td>";
+            echo "</tr>";
+            echo "</form>";
+        
+        }
+        
+        echo "</table>";
+            ?>
+
+
+<h2>Shop Manager</h2>
+        <?php
+        echo "<table border = 1>";
+        echo  '<th>First Name</th><th>Last Name</th> <th>email</th><td>phone</th><td>address</td><td>licenseNumber<td>';
+        foreach($shopManagerData as $doc) {
+            echo "<form>";
+            echo "<tr>";
+            echo "<td>".$doc['firstName']."</td>";
+            echo "<td>".$doc['lastName']."</td>";
+            echo "<td>".$doc['email']."</td>";
+            echo "<td>".$doc['phone']."</td>";
+            echo "<td>".$doc['address']."</td>";
+            echo "<td>".$doc['licenseNumber']."</td>";
+            $email = $doc['email'];
+            echo "<td> <button type='submit' value='$email'> edit</button> </td>";
+            echo "</tr>";
+            echo "</form>";
+        
+        }
+        
+        echo "</table>";
+            ?>
+
+   
 </body>
 </html>
