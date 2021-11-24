3dc9153c163f833fd94ab11eb3d20ee9f24b975f
S A Saharukh <sasaharukh@gmail.com>
2021-11-25 03:38:51 +0600

    header footer added

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,59 @@
+
+<link rel="stylesheet" href="./css/footer.css">
 <footer>
-  <hr>
-    <p>made with ❤️ by <a href="https://saharukh.com">Saharukh</a> </p>
-   
-  </footer>
+        <section class="ft-main">
+            <div class="ft-main-item">
+                <h2 class="ft-title">About</h2>
+                <ul>
+                   <li> Doctor’s patient is always dedicated</li>
+                   <li> and determined towards providing the </li>
+                   <li> best experience for it’s users. We provide </li>   
+                   <li> a one-stop solution for all your needs  </li>
+                   <li> starting from online consultancy and </li> 
+                   <li> prescription to ordering medicines online.</li>
+                </ul>
+            </div>
+            <div class="ft-main-item">
+                <h2 class="ft-title">Our Services</h2>
+                <ul>
+                    <li>Online Consultation</li>
+                    <li>Expert Doctors</li>
+                    <li>Online Prescription</li>
+                    <li>Online Payment</li>
+                    <li>Online Appointment</li>
+                    <li>Online Medicine Order</li>
+                </ul>
+            </div>
+            <div class="ft-main-item">
+                <h2 class="ft-title">Contact Us</h2>
+                <ul>
+                    <li>Admin Department</li>
+                    <li>+8801750-729276</li>
+                    <li>Email</li>
+                    <li>sasaharukh@gmail.com</li>
+                    <li>HR Department</li>
+                    <li>+8801789-171191</li>
+
+                </ul>
+        </section>
+
+        <section class="ft-social">
+            <ul class="ft-social-list">
+                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-github"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+        </section>
+
+        <section class="ft-legal">
+            <ul class="ft-legal-list">
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Terms of Use</a></li>
+                <li>Copyright ©<?php echo date('Y')?>. All Rights Reserved.Designed By ABC IT Solution</li>
+            </ul>
+        </section>
+
+    </footer>
